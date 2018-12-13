e3b0f42a9d83e641574f9b6eeb42a0e8f8e0ef7c
Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>
2018-12-13 20:58:31 +0200

    Fix errors
    Signed-off-by: Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>

--- include/undefined.h
+++ include/undefined.h
@@ -17,10 +17,17 @@ public:
 class D : public C{};
 
 class E : virtual public B {
-private:
-    int omg() {{ std::cout << __PRETTY_FUNCTION__ << std::endl; }}
+protected:
+    int f2() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
 };
 
-class F: public D, public E{};
+class F: public D, public E{
+public:
+    int f2() {
+        E::f2();
+        C::f2();
+    }
+
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -83,17 +83,21 @@ int main() {
     //using namespace Workers;
     using namespace VirtualWorkers;
 
-    Waiter bob("Bob Apple", 314L, 5);
-    Singer bev("Beverly Hills", 522L, 3);
+    Waiter waiter("Vassily Popkine", 123L, 4);
+    Singer singer("Ivan Piotr", 666L, 3);
     Waiter w_temp;
     Singer s_temp;
-    Worker* pw[4] = { &bob, &bev, &w_temp, &s_temp };
+    Worker* pw[4] = { &waiter, &singer, &w_temp, &s_temp };
     int i{};
     for (i = 2; i < 4; i++) pw[i]->set();
     for (i = 0; i < 4; i++) {
         pw[i]->show();
         std::cout << std::endl;
     }
+
+    SingingWaiter sw;
+    sw.set();
+    sw.show();
 
     F f;
     f.f1();
