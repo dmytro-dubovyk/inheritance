f1625028e61930068224b160f2f1762e3e43d804
Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>
2020-06-16 23:39:52 +0300

    Split the examples into 2 branches
    Signed-off-by: Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,16 @@
 #include <vector>
 
 #include "circle.h"
-//#include "engine.h"
 #include "mybaseclass.h"
 #include "student_private.h"
 #include "student_public.h"
-#include "vehicle.h"
 #include "virtual_worker.h"
 #include "worker.h"
 #include "undefined.h"
 
 int main() {
-// Block #1
-    std::cout << "Creating car #1:" << std::endl;
-    Car car1;
-    std::cout << "Creating car #2:" << std::endl;
-    Car car2("BMW");
-    std::cout << "Creating bus #1:" << std::endl;
-    Bus bus;
-    std::cout << "Creating a bus #2:" << std::endl;
-    DieselBus dieselBus;
-
-    std::cout << "Creating a link to car #1:" << std::endl;
-    Vehicle& pV = car1; 
-    pV.drive();
-
-    std::cout << "--------------------" << std::endl;
-
-    FourWheelerCar fourWheelerCar;
-    FourWheeler* fw = dynamic_cast<FourWheeler*>(&fourWheelerCar);
-    fw->drive();
-
-    Vehicle* vehicle = dynamic_cast<Vehicle*>(&fourWheelerCar);
-    vehicle->drive();
-
-    std::cout << "--------------------" << std::endl;
-    std::vector<Vehicle*> vec{ &car1, &car2, &bus, &dieselBus };
-    for(auto vehicle_ : vec) {
-        vehicle_->drive();
-        vehicle_->stop();
-    }
-    std::cout << "--------------------" << std::endl;
-
-
 // Block #2
-/*
+
     Student student("Vassily Poupkine", 10);
     std::cout << student;
     student = Student(5);
@@ -56,18 +22,8 @@ int main() {
     st = StudentPrivate(10);
     std::cout << st;
 
-    Car car;
-    car.start();
-
-    AnotherCar car2;
-    car2.start();
-
-    BigCar car3;
-    car3.start();
-*/
-
 // Block #3
-/*
+
     CircleInherited circle1(10.0f);
     circle1.setRadius(12.0);
     std::cout << "Radius #1 = " << circle1.getRadius()
@@ -80,10 +36,6 @@ int main() {
 
     Derived d;
     d.processData();
-
-
-    BigCar car;
-    car.start();
 
     //using namespace Workers;
     using namespace VirtualWorkers;
@@ -107,6 +59,6 @@ int main() {
     F f;
     f.f1();
     f.f2();
-*/
+
     return 0;
 }
