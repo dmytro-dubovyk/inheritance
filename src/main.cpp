8af0d2d248791add35b16a5ec38d0a8e2e81d99a
Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>
2020-06-21 21:41:43 +0300

    Get rid of extra stuff for example #2
    Signed-off-by: Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 #include "mybaseclass.h"
 #include "student_private.h"
 #include "student_public.h"
-#include "virtual_worker.h"
-#include "worker.h"
-#include "undefined.h"
 
 int main() {
 // Block #2
@@ -36,29 +33,6 @@ int main() {
 
     Derived d;
     d.processData();
-
-    //using namespace Workers;
-    using namespace VirtualWorkers;
-
-    Waiter waiter("Vassily Popkine", 123L, 4);
-    Singer singer("Ivan Piotr", 666L, 3);
-    Waiter w_temp;
-    Singer s_temp;
-    Worker* pw[4] = { &waiter, &singer, &w_temp, &s_temp };
-    int i{};
-    for (i = 2; i < 4; i++) pw[i]->set();
-    for (i = 0; i < 4; i++) {
-        pw[i]->show();
-        std::cout << std::endl;
-    }
-
-    SingingWaiter sw;
-    sw.set();
-    sw.show();
-
-    F f;
-    f.f1();
-    f.f2();
 
     return 0;
 }
